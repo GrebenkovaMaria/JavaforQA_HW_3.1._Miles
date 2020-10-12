1582a4d16e7c5aef952002e31a8d0e4f1d7e8d6f
Maria Grebenkova <grebenkova.m.p@gmail.com>
2020-10-12 23:00:33 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        long ticket_price ; //with penny
+        int mile_price ;
+        long total_miles ;
+        mile_price = 20 ;
+        ticket_price = 2099;
+        total_miles = ticket_price/100/mile_price ;
+        System.out.println(total_miles);
+    }
+}
